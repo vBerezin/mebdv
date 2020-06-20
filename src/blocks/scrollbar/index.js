bb62078dbd235323c1682fe0d2936dbcfebbf8f7
v.berezin <v.berezin@corp.mail.ru>
2020-06-20 23:04:05 +0300

    scrollbar

--- src/blocks/scrollbar/index.js
+++ src/blocks/scrollbar/index.js
@@ -1,24 +1,25 @@
 // TODO: touch drag
 
-export class Scrollbar {
+class Proto {
+  constructor() {
+    this.instances.push(this);
+  }
+}
+
+Proto.prototype.instances = [];
+
+class Scrollbar extends Proto{
   constructor({ node, container }) {
+    super();
     this.el = node;
     this.container = container;
     this.track = this.el.querySelector('[data-rel="scrollbar.track"]');
     this.container.addEventListener('scroll', () => {
       this.move(this.container.scrollLeft / this.container.offsetWidth);
     }, { passive: true });
-    window.addEventListener('resize', () => {
-      this.init();
-    });
     this.init();
   }
-  set active(state) {
-    this.container.classList.toggle('scrollbar-active', state);
-    this.el.classList.toggle('is-active', state);
-  }
   init() {
-    this.scrollSize = this.container.scrollWidth - this.container.offsetWidth;
     this.ratio = this.container.offsetWidth / this.container.scrollWidth;
     this.track.style.width = `${this.ratio * 100}%`;
     this.move(this.container.scrollLeft / this.container.offsetWidth);
@@ -27,4 +28,14 @@ export class Scrollbar {
   move(position) {
     this.track.style.transform = `translateX(${position * 100}%)`;
   }
+  set active(state) {
+    this.container.classList.toggle('scrollbar-active', state);
+    this.el.classList.toggle('is-active', state);
+  }
 }
+
+window.addEventListener('resize', () => {
+  Scrollbar.prototype.instances.forEach(instance => instance.init());
+});
+
+export { Scrollbar };
